9f977fa5493515a91ac1dbe0c7774eada3fcad77
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-05 00:45:20 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,16 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
-def remove_strawberry(contacts)
 
+def remove_strawberry(contacts)
+    contacts.map {|person, person_inf|
+        person_inf.map{|info_keys,value|
+            if info_keys == :favorite_ice_cream_flavors
+                value.delete_if {|ice_cream|
+                   ice_cream == "strawberry"
+              }
+              end
+        }
+   }
+   contacts
 end
